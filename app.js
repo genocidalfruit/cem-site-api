2cf72b7fdf095a406d6f26a3467fbeb98490cb2c
surxjb <85881249+genocidalfruit@users.noreply.github.com>
2025-06-09 17:56:10 +0530

    cors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors());
 
 // Connect to DB
 connectDB();
